bab291226ac063c6e935606f3225753b24e9e5c2
Vitaliy Ugera <shnobel12@gmail.com>
2022-06-21 18:43:56 +0300

    Добавьте файлы проекта.

---
+++ Homework1Part1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1Part1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("The square's side value : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int area = a * a;
+            int perimeter = 4 * a;
+            Console.WriteLine($"The area of the square with length {a} is {area} and the perimeter is {perimeter}");
+        }
+    }
+}
