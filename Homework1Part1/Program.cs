8f72b4169a62b297438985ace8aa98e387f4948a
Vitaliy Ugera <shnobel12@gmail.com>
2022-06-23 13:49:50 +0300

    Updated v1.1

--- Homework1Part1/Program.cs
+++ Homework1Part1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Homework1Part1
 {
@@ -10,11 +6,26 @@ namespace Homework1Part1
     {
         static void Main(string[] args)
         {
-            Console.Write("The square's side value : ");
-            int a = Convert.ToInt32(Console.ReadLine());
+            //Homework1Task1
+            Console.Write("Write the square's side value : ");
+            string s = Console.ReadLine();
+            string s1 = string.Empty; //string of numbers to convert into Int32
+            foreach (char value in s)
+            {
+                if (char.IsNumber(value))
+                {
+                    s1 = string.Concat(s1, value);
+                }
+            }
+            int a = Convert.ToInt32(s1); //square's side value
             int area = a * a;
             int perimeter = 4 * a;
-            Console.WriteLine($"The area of the square with length {a} is {area} and the perimeter is {perimeter}");
+            Console.WriteLine($"The area of the square with side length {a} is {area} and its perimeter equals {perimeter}\n");
+            Console.WriteLine("Press 'ENTER' to go to the next Task... ");
+            while (Console.ReadKey(true).Key != ConsoleKey.Enter)
+            {
+
+            }
         }
     }
 }
